3f0001187850be79c418d577f61a4c4458419862
Rohit Sharma <rohit2sharma95@gmail.com>
2020-05-17 10:29:28 +0530

    Update top navbar to add more buttons

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
 const Dashboard = () => (
-  <div className="container text-center">
-    <div className="py-5" />
-    <h1>Welcome, Rodney Gibson!</h1>
-    <p className="lead">
-      Thanks for your patience :) There is nothing to show here yet as
-      { ' ' }
-      <br />
-      <span className="font-italic">This page is under construction.</span>
-    </p>
+  <div className="container text-center py-5">
+    *****
   </div>
 );
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+import { Dropdown } from 'react-bootstrap';
+import { Bell, Search } from 'react-feather';
 
 const Navbar = () => (
-  <nav className="navbar navbar-light flex-md-nowrap py-0">
-    <span className="h1 font-weight-light">Hello Brad</span>
-    <ul className="navbar-nav px-3">
-      <li className="nav-item text-nowrap">
-        <Link className="nav-link" to="/">Sign out</Link>
-      </li>
-    </ul>
-  </nav>
+  <div className="border-bottom border-light pb-3">
+    <nav className="navbar navbar-light navbar-expand-lg">
+      <span className="d-inline-flex flex-column">
+        <span className="h1 font-weight-light">
+          Hello Brad
+        </span>
+        <span className="font-weight-light text-muted lead role-text">Admin</span>
+      </span>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item text-nowrap mr-4">
+          <Link className="nav-link border border-primary rounded py-1" to="/" title="Get help">Help</Link>
+        </li>
+        <li className="nav-item dropdown">
+          <Dropdown>
+            <Dropdown.Toggle variant="outline-primary" id="dropdown-basic" className="user-dropdown rounded-circle p-0" title="Brad Thomas">
+              <img src="https://randomuser.me/api/portraits/men/77.jpg" alt="Brad Thomas" className="rounded-circle" />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Link className="dropdown-item" to="/dashboard">Employees</Link>
+              <Link className="dropdown-item" to="/dashboard">Supervisors</Link>
+              <Link className="dropdown-item" to="/dashboard">Human Resource Managers</Link>
+            </Dropdown.Menu>
+          </Dropdown>
+        </li>
+      </ul>
+    </nav>
+    <div className="container-fluid">
+      <div className="btn-toolbar justify-content-end" role="toolbar" aria-label="Toolbar with button groups">
+        <div className="btn-group mr-2" role="group" aria-label="Second group">
+          <button type="button" className="btn btn-link text-secondary">
+            <Search size={ 20 } />
+          </button>
+          <button type="button" className="btn btn-link text-secondary">
+            <Bell size={ 20 } />
+          </button>
+        </div>
+        <div className="btn-group" role="group" aria-label="Third group">
+          <button type="button" className="btn btn-primary">Apply For a Leave</button>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Navbar;
