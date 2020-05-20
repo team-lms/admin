5693f2e9951d6a913408fbda0650689e1e80fcf6
Siju Samson <sijusamson@gmail.com>
2020-05-20 22:39:14 +0530

    Created Services
    Started integrating services for Employee Listing

---
+++ src/api/service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3030/api/v1';
+export const Employee = {
+  getEmployeeList: async (filters) => {
+    try {
+      const result = await axios.get('/employee', {
+        params: {
+          offset: filters.offset,
+          limit: filters.limit,
+          sortType: filters.sortType,
+          sortField: filters.sortField
+        }
+      });
+      return result;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+};
+
+export default {
+  Employee
+};
