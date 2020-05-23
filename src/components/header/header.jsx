68c482c70e365937747828c91ba2a0e2f8563cf3
Siju Samson <sijusamson@gmail.com>
2020-05-23 21:21:53 +0530

    Resolved Conflict

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,23 +3,21 @@ import { Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Header = ({ selectedPage }) => {
-  Header.propTypes = {
-    selectedPage: PropTypes.string.isRequired
-  };
-  return (
-    <>
-      <Dropdown>
-        <Dropdown.Toggle variant="secondary" id="dropdown-basic" className="btn-sm">
-          {selectedPage}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Link className="dropdown-item" to="/employee/list">Employees</Link>
-          <Link className="dropdown-item" to="/supervisor/list">Supervisors</Link>
-          <Link className="dropdown-item" to="/humanresource/list">Human Resource Managers</Link>
-        </Dropdown.Menu>
-      </Dropdown>
-    </>
-  );
+const Header = ({ selectedPage }) => (
+  <>
+    <Dropdown>
+      <Dropdown.Toggle variant="secondary" id="dropdown-basic" className="btn-sm">
+        {selectedPage}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Link className="dropdown-item" to="/employee/list">Employees</Link>
+        <Link className="dropdown-item" to="/supervisor/list">Supervisors</Link>
+        <Link className="dropdown-item" to="/humanresource/list">Human Resource Managers</Link>
+      </Dropdown.Menu>
+    </Dropdown>
+  </>
+);
+Header.propTypes = {
+  selectedPage: PropTypes.string.isRequired
 };
 export default Header;
