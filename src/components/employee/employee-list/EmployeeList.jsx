e7bc86258234f8e87011e9fec0b4726f67f8eebe
Siju Samson <sijusamson@gmail.com>
2020-05-23 10:45:16 +0530

    Employee Listing
    API Integrated for Get Employee Listing

--- src/components/employee/employee-list/EmployeeList.jsx
+++ src/components/employee/employee-list/EmployeeList.jsx
@@ -14,14 +14,14 @@ const EmployeeList = () => {
 
   const getEmployeeList = async () => {
     const result = await Employee.getEmployeeList(filters);
-    if (result.success) {
-      setEmployees([result.data]);
+    if (result.data.success) {
+      const newData = result.data.data.rows;
+      setEmployees(...employees, newData);
     }
   };
 
   useEffect(() => {
     getEmployeeList();
-    setEmployees([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]);
   }, []);
 
 
@@ -60,64 +60,68 @@ const EmployeeList = () => {
           </thead>
           <tbody>
             {
-              employees.map((employee, index) => (
-                <tr key={ employee.id }>
-                  <td className={ index === 0 ? 'border-top-0' : '' }>
-                    <span className="d-block">
-                      Brad Thomas
-                    </span>
-                    <small className="text-muted">
-                      (Since
-                      {' '}
-                      {moment(employee.createdAt).format('Do MMM YYYY')}
-                      )
-                    </small>
-                  </td>
-                  <td className={ index === 0 ? 'border-top-0' : '' }>
-                    <span className="d-inline-block">
+                employees.map((employee, index) => (
+                  <tr key={ employee.id }>
+                    <td className={ index === 0 ? 'border-top-0' : '' }>
                       <span className="d-block">
-                        rose.pelletier@example.com
+                        {employee.firstName}
+                        {' '}
+                        {' '}
+                        {employee.lastName}
                       </span>
-                      <small className="text-muted">045-672-3124</small>
-                    </span>
-                  </td>
-                  <td className={ index === 0 ? 'border-top-0' : '' }>
-                    <span className="d-block">
-                      Sally Pena
-                    </span>
-                    <small className="text-muted">(Technical Head)</small>
-                  </td>
-                  <td className={ index === 0 ? 'border-top-0' : '' }>
-                    <OverlayTrigger
-                      trigger="click"
-                      placement="bottom"
-                      rootClose="false"
-                      overlay={
-                        (
-                          <Popover id={ `popover-positioned-${employee.id}` }>
-                            <Popover.Content bsPrefix="popover-body p-0 overflow-hidden rounded">
-                              <div className="list-group list-group-flush rounded">
-                                <Link className="list-group-item list-group-item-action py-1 px-2" to="/employee">Edit</Link>
-                                <Link className="list-group-item list-group-item-action py-1 px-2" to="/">Delete</Link>
-                              </div>
-                            </Popover.Content>
-                          </Popover>
+                      <small className="text-muted">
+                        (Since
+                        {' '}
+                        {moment(employee.createdAt).format('Do MMM YYYY')}
                         )
-                      }
-                    >
-                      <button type="button" className="btn  btn-sm py-0">
-                        <img className="path" src={ action } alt="action icon" />
-                      </button>
-                    </OverlayTrigger>
-                  </td>
-                </tr>
-              ))
-            }
+                      </small>
+                    </td>
+                    <td className={ index === 0 ? 'border-top-0' : '' }>
+                      <span className="d-inline-block">
+                        <span className="d-block">
+                          {employee.email}
+                        </span>
+                        <small className="text-muted">{employee.phoneNumber}</small>
+                      </span>
+                    </td>
+                    <td className={ index === 0 ? 'border-top-0' : '' }>
+                      <span className="d-block">
+                        Sally Pena
+                      </span>
+                      <small className="text-muted">(Technical Head)</small>
+                    </td>
+                    <td className={ index === 0 ? 'border-top-0' : '' }>
+                      <OverlayTrigger
+                        trigger="click"
+                        placement="bottom"
+                        rootClose="false"
+                        overlay={
+                          (
+                            <Popover id={ `popover-positioned-${employee.id}` }>
+                              <Popover.Content bsPrefix="popover-body p-0 overflow-hidden rounded">
+                                <div className="list-group list-group-flush rounded">
+                                  <Link className="list-group-item list-group-item-action py-1 px-2" to="/employee">Edit</Link>
+                                  <Link className="list-group-item list-group-item-action py-1 px-2" to="/">Delete</Link>
+                                </div>
+                              </Popover.Content>
+                            </Popover>
+                          )
+                        }
+                      >
+                        <button type="button" className="btn  btn-sm py-0">
+                          <img className="path" src={ action } alt="action icon" />
+                        </button>
+                      </OverlayTrigger>
+                    </td>
+                  </tr>
+                ))
+              }
           </tbody>
         </table>
       </div>
     </>
   );
 };
+
 
 export default EmployeeList;
