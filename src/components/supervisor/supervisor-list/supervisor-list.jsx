6929d458c395046a1b7aad125c264e0631f6ac75
Siju Samson <sijusamson@gmail.com>
2020-07-25 10:46:59 +0530

    Supervisor List Completed

--- src/components/supervisor/supervisor-list/supervisor-list.jsx
+++ src/components/supervisor/supervisor-list/supervisor-list.jsx
@@ -122,7 +122,8 @@ const SupervisorList = ({ history }) => {
                   <td className={ index === 0 ? 'border-top-0' : '' }>
                     <span className="d-inline-block">
                       <span className="d-block">
-                        { supervisor.teamAssociation && supervisor.teamAssociation.team.teamName }
+                        { supervisor.team
+                          && supervisor.team.teamName }
                       </span>
                     </span>
                   </td>
