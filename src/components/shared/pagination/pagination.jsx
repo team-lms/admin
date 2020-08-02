e80edfc31cbc46b522a3619dc9b55ea77698d073
Rohit Sharma <rohit2sharma95@gmail.com>
2020-08-02 09:50:29 +0530

    Fix issues in pagination
    - Complete Pagination component
    - Complete pagination in employee listing component

--- src/components/shared/pagination/pagination.jsx
+++ src/components/shared/pagination/pagination.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-const PaginationComponent = ({ number }) => (
-  <Pagination.Item key={ number } active={ number }>
-    <h1>111</h1>
-    { number }
-  </Pagination.Item>
+const PaginationComponent = ({ totalPage, activePage, paginate }) => (
+  <Pagination>
+    <Pagination.First onClick={ (e) => paginate(e, 1) } />
+    <Pagination.Prev onClick={ (e) => paginate(e, activePage - 1) } />
+    { [...Array(totalPage).keys()].map((pageIndex) => {
+      const pageNumber = pageIndex + 1;
+      return (
+        <Pagination.Item
+          active={ pageNumber === activePage }
+          onClick={ (e) => paginate(e, pageNumber) }
+        >
+          { pageNumber }
+        </Pagination.Item>
+      );
+    }) }
+    <Pagination.Next onClick={ (e) => paginate(e, activePage + 1) } />
+    <Pagination.Last onClick={ (e) => paginate(e, totalPage) } />
+  </Pagination>
 );
+
+PaginationComponent.propTypes = {
+  totalPage: PropTypes.number.isRequired,
+  activePage: PropTypes.number.isRequired,
+  paginate: PropTypes.func.isRequired
+};
 
 export default PaginationComponent;
