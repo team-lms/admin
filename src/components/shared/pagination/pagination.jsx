aaf5c6d4dbb70b7e574f3839717fae9fbb0911ea
Rohit Sharma <rohit2sharma95@gmail.com>
2020-08-02 10:36:54 +0530

    Update the Pagination componnet to add core bootstrap's component

--- src/components/shared/pagination/pagination.jsx
+++ src/components/shared/pagination/pagination.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import { Pagination } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const PaginationComponent = ({ totalPage, activePage, paginate }) => (
-  <Pagination>
-    <Pagination.First onClick={ (e) => paginate(e, 1) } />
-    <Pagination.Prev onClick={ (e) => paginate(e, activePage - 1) } />
-    { [...Array(totalPage).keys()].map((pageIndex) => {
-      const pageNumber = pageIndex + 1;
-      return (
-        <Pagination.Item
-          active={ pageNumber === activePage }
-          onClick={ (e) => paginate(e, pageNumber) }
-        >
-          { pageNumber }
-        </Pagination.Item>
-      );
-    }) }
-    <Pagination.Next onClick={ (e) => paginate(e, activePage + 1) } />
-    <Pagination.Last onClick={ (e) => paginate(e, totalPage) } />
-  </Pagination>
+
+  <nav aria-label="Page navigation example">
+    <ul className="pagination">
+      <li className="page-item">
+        <a className="page-link" href="#" aria-label="First" onClick={ (e) => paginate(e, 1) }>
+          <span aria-hidden="true">«</span>
+        </a>
+      </li>
+      <li className="page-item">
+        <a className="page-link" href="#" aria-label="Previous" onClick={ (e) => paginate(e, (activePage > 1) ? (activePage - 1) : 1) }>
+          <span aria-hidden="true">‹</span>
+        </a>
+      </li>
+      { [...Array(totalPage).keys()].map((pageIndex) => {
+        const pageNumber = pageIndex + 1;
+        return (
+          <li className={ `page-item ${pageNumber === activePage ? 'active' : ''}` }>
+            <a className="page-link" href="#" onClick={ (e) => paginate(e, pageNumber) }>
+              { pageNumber }
+            </a>
+          </li>
+        );
+      }) }
+      <li className="page-item">
+        <a className="page-link" href="#" aria-label="Next" onClick={ (e) => paginate(e, (activePage < totalPage) ? (activePage + 1) : totalPage) }>
+          <span aria-hidden="true">›</span>
+        </a>
+      </li>
+      <li className="page-item">
+        <a className="page-link" href="#" aria-label="Last" onClick={ (e) => paginate(e, totalPage) }>
+          <span aria-hidden="true">»</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 );
 
 PaginationComponent.propTypes = {
