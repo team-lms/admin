c40118bfd9c7d5d8a7590c99be642a31ea0c8652
Siju Samson <sijusamson@gmail.com>
2021-02-07 19:07:41 +0530

    Logo added for LMS

--- src/components/side-nav/SideNav.jsx
+++ src/components/side-nav/SideNav.jsx
@@ -5,26 +5,41 @@ import {
   User,
   Settings
 } from 'react-feather';
+import logo from "../../assets/img/logo.png";
 
 const SideNav = () => (
   <nav className="sidebar-sticky sidebar bg-light">
-    <Link className="d-block logo text-decoration-none py-4 text-center" to="/">LMS</Link>
+    <Link className="d-block logo text-decoration-none py-4 ht-40 text-center" to="/">
+      <img src={logo} className="img-fluid" alt="Dashboard-logo" />
+    </Link>
     <ul className="nav flex-column">
       <li className="nav-item text-center">
-        <NavLink className="nav-link d-inline-flex flex-column justify-content-center align-items-center rounded-lg" activeClassName="active shadow" to="/dashboard">
-          <PieChart size={ 32 } />
+        <NavLink
+          className="nav-link d-inline-flex flex-column justify-content-center align-items-center rounded-lg"
+          activeClassName="active shadow"
+          to="/dashboard"
+        >
+          <PieChart size={32} />
           <span>Dashboard</span>
         </NavLink>
       </li>
       <li className="nav-item text-center mt-4">
-        <NavLink className="nav-link d-inline-flex flex-column justify-content-center align-items-center rounded-lg" activeClassName="active shadow" to="/profile">
-          <User size={ 32 } />
+        <NavLink
+          className="nav-link d-inline-flex flex-column justify-content-center align-items-center rounded-lg"
+          activeClassName="active shadow"
+          to="/profile"
+        >
+          <User size={32} />
           <span>Profile</span>
         </NavLink>
       </li>
       <li className="nav-item text-center mt-4">
-        <NavLink className="nav-link d-inline-flex flex-column justify-content-center align-items-center rounded-lg" activeClassName="active shadow" to="/settings">
-          <Settings size={ 32 } />
+        <NavLink
+          className="nav-link d-inline-flex flex-column justify-content-center align-items-center rounded-lg"
+          activeClassName="active shadow"
+          to="/settings"
+        >
+          <Settings size={32} />
           <span>Settings</span>
         </NavLink>
       </li>
